1e3b5dbebfabce5c635e03465e04b530588d4e89
rushikesh <153095920+Shailesh2302@users.noreply.github.com>
2025-12-14 15:42:44 +0530

    modified to monorepo

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import { Card } from "@repo/ui/card";
+import { Gradient } from "@repo/ui/gradient";
+import { TurborepoLogo } from "@repo/ui/turborepo-logo";
+import { prisma } from "@repo/db";
+
+export default async function Page() {
+  const user = await prisma.user.findFirst();
+
+  return (
+    <main className="flex flex-col items-center justify-between min-h-screen p-24">
+      {user?.github_id}
+    </main>
+  );
+}
